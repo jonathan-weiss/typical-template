17ec33cbc18fe1d7315198827d9f5d6d7ec13261
Jonathan Weiss <jonathan.weiss@gmx.ch>
2025-08-14 17:58:09 +0200

    Fix wrong file pattern for scss files

--- typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/filemapping/FileEndings.kt
+++ typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/filemapping/FileEndings.kt
@@ -4,5 +4,5 @@ object FileEndings {
     val HTML_FILENAME_REGEX: Regex = Regex(".*\\.(html|xhtml)")
     val KOTLIN_FILENAME_REGEX: Regex = Regex(".*\\.kt")
     val TYPESCRIPT_FILENAME_REGEX: Regex = Regex(".*\\.ts")
-    val SCSS_FILENAME_REGEX: Regex = Regex(".*\\.ts")
+    val SCSS_FILENAME_REGEX: Regex = Regex(".*\\.scss")
 }
