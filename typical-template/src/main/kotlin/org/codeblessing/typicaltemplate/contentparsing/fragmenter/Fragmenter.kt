2b33a34d274464dc773e61e2f3d1925c6cc8eaa8
Jonathan Weiss <jonathan.weiss@gmx.ch>
2026-04-21 14:44:48 +0200

    Reduce scope of token variable

--- typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/contentparsing/fragmenter/Fragmenter.kt
+++ typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/contentparsing/fragmenter/Fragmenter.kt
@@ -12,8 +12,7 @@ object Fragmenter {
     fun createFragmentsFromTokens(tokensWithMetadata: List<TokenWithMetadata>): List<TemplateFragment> {
         return tokensWithMetadata.flatMap { tokenWithMetadata ->
             val lineNumbers = LineNumberCalculator.calculateLineNumbers(tokenWithMetadata, tokensWithMetadata)
-            val token = tokenWithMetadata.token
-            when (token) {
+            when (val token = tokenWithMetadata.token) {
                 is PlainContentToken -> listOf(FragmentFactory.createTextFragment(
                     text = token.value,
                     lineNumbers = lineNumbers
