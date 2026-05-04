622b86f50e25513693e61a8a9f3f57dbf6204bb5
Jonathan Weiss <jonathan.weiss@gmx.ch>
2026-05-04 16:44:07 +0200

    Add mutualExclusiveAttributes property

--- typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/AttributeGroupConstraint.kt
+++ typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/AttributeGroupConstraint.kt
@@ -4,6 +4,7 @@ data class AttributeGroupConstraint(
     val occurrence: AttributeGroupOccurrence,
     val requiredAttributes: Set<CommandAttributeKey> = emptySet(),
     val optionalAttributes: Set<CommandAttributeKey> = emptySet(),
+    val mutualExclusiveAttributes: Set<CommandAttributeKey> = emptySet(),
 ) {
     val allowedAttributes: Set<CommandAttributeKey> = requiredAttributes + optionalAttributes
 }
