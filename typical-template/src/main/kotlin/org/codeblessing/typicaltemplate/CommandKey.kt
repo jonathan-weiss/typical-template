912c12e5fd9aa0a9bc219965026398f87a15aa19
Jonathan Weiss <jonathan.weiss@gmx.ch>
2026-05-05 13:16:14 +0200

    Add keyword type to the CommandKey properties

--- typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/CommandKey.kt
+++ typical-template/src/main/kotlin/org/codeblessing/typicaltemplate/CommandKey.kt
@@ -7,6 +7,7 @@ import org.codeblessing.typicaltemplate.CommandAttributeKey.*
 
 enum class CommandKey(
     val keyword: String,
+    val keywordType: KeywordType = KeywordType.COMMAND,
     val attributeGroupConstraints: List<AttributeGroupConstraint> = emptyList(),
     val correspondingOpeningCommandKey: CommandKey? = null,
     val directlyNestedInsideCommandKey: CommandKey? = null,
@@ -136,6 +137,7 @@ enum class CommandKey(
     ),
     MOVE_COMMENT(
         keyword = "move-comment",
+        keywordType = KeywordType.PREPROCESSOR_COMMAND,
         attributeGroupConstraints = listOf(
             AttributeGroupConstraint(
                 occurrence = ONE_ATTRIBUTE_GROUP,
@@ -147,6 +149,7 @@ enum class CommandKey(
     ),
     EXPAND_COMMENT(
         keyword = "expand-comment",
+        keywordType = KeywordType.PREPROCESSOR_COMMAND,
         attributeGroupConstraints = listOf(
             AttributeGroupConstraint(
                 occurrence = ONE_ATTRIBUTE_GROUP,
