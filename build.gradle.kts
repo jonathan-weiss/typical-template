ed44b8840db690d2580c8176cd2d8e7f7f69d4b1
Jonathan Weiss <jonathan.weiss@gmx.ch>
2026-05-07 11:29:02 +0200

    Fix dependencies of the version update process

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,21 +5,22 @@ tasks.register("generateDocumentation") {
 
 tasks.register("updateVersionString") {
     val readmeMdFile: File = rootProject.file("README.md")
+    val newVersion = project.property("typicaltemplate.version") as String
 
     doLast {
-        replaceVersionString(readmeMdFile)
+        replaceVersionString(readmeMdFile, newVersion)
     }
 
+    inputs.property("version", newVersion)
     inputs.file(readmeMdFile)
     outputs.file(readmeMdFile)
 }
 
-fun replaceVersionString(file: File) {
-    val version = project.property("typicaltemplate.version") as String
+fun replaceVersionString(file: File, newVersion: String) {
     val content = file.readText()
     val newContent = content
-        .replace(Regex("typical-template-api:\\d+\\.\\d+\\.\\d+"), "typical-template-api:$version")
-        .replace(Regex("typical-template:\\d+\\.\\d+\\.\\d+"), "typical-template:$version")
+        .replace(Regex("typical-template-api:\\d+\\.\\d+\\.\\d+"), "typical-template-api:$newVersion")
+        .replace(Regex("typical-template:\\d+\\.\\d+\\.\\d+"), "typical-template:$newVersion")
 
     file.writeText(newContent)
 }
