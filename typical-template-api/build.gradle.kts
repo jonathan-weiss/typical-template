dfb72e3545da65df1fc1d21c9ae90d6bffbe67de
Jonathan Weiss <jonathan.weiss@gmx.ch>
2026-05-09 15:01:38 +0200

    Add manifest to typical-template-api main function jar

--- typical-template-api/build.gradle.kts
+++ typical-template-api/build.gradle.kts
@@ -3,3 +3,11 @@ plugins {
     `typical-template-publishing`
     `maven-dependency-repository`
 }
+
+tasks.jar {
+    val typicalTemplateVersion = project.property("typicaltemplate.version") as String
+    manifest {
+        attributes("Main-Class" to "org.codeblessing.typicaltemplate.TypicalTemplateKt")
+        attributes("Implementation-Version" to typicalTemplateVersion)
+    }
+}
