a3b2253031ded3ad35e136e3722879f5488eaae9
Jonathan Weiss <jonathan.weiss@gmx.ch>
2026-06-19 18:45:59 +0200

    Remove test (to be replaced by blackbox test)

--- typical-template/src/test/kotlin/org/codeblessing/typicaltemplate/templaterenderer/TemplateRendererTest.kt
+++ typical-template/src/test/kotlin/org/codeblessing/typicaltemplate/templaterenderer/TemplateRendererTest.kt
@@ -35,40 +35,4 @@ class TemplateRendererTest {
         println("--------------------------------------")
         assertEquals(expectedContent, kotlinTemplateRendererClassContent)
     }
-
-    @Test
-    fun `create template content from file with nested template-renderers`() {
-        val contentToParse = ClasspathResourceLoader.loadClasspathResource(
-            classpathResourcePath = "org/codeblessing/typicaltemplate/templaterenderer/TemplateRendererTest-nested-content-to-parse.kt.txt",
-        )
-
-        val templates = ContentParser.parseContent(content = contentToParse, KOTLIN_COMMENT_STYLES)
-
-        assertEquals(3, templates.size)
-
-        val expectedFiles = listOf(
-            "TemplateRendererTest-nested-expected-outer.txt",
-            "TemplateRendererTest-nested-expected-inner.txt",
-            "TemplateRendererTest-nested-expected-deep.txt",
-        )
-        val filepathStrings = listOf(
-            RelativeFile.fromRelativeString("dummy-dir/author-subdir/dummy-Author.txt"),
-            RelativeFile.fromRelativeString("dummy-dir/inner.txt"),
-            RelativeFile.fromRelativeString("dummy-dir/deep.txt"),
-        )
-
-        templates.forEachIndexed { index, template ->
-            val kotlinTemplateContent = TemplateRendererContentCreator.createMultilineStringTemplateContent(filepathStrings[index], template)
-            val kotlinTemplateRendererClassContent = TemplateRendererClassContentCreator.wrapInKotlinClassContent(filepathStrings[index], template, kotlinTemplateContent)
-
-            println("--- Kotlin Template [${template.templateRendererClass.className}] ------------------")
-            println(kotlinTemplateRendererClassContent)
-            println("--------------------------------------")
-
-            val expectedContent = ClasspathResourceLoader.loadClasspathResource(
-                classpathResourcePath = "org/codeblessing/typicaltemplate/templaterenderer/${expectedFiles[index]}",
-            )
-            assertEquals(expectedContent, kotlinTemplateRendererClassContent)
-        }
-    }
 }
